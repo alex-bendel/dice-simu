354ac4ccb200039926bc6c2c46087434f6ad7d66
Alex B <albend@gmail.com>
2011-04-02 03:56:43 +0200

    added animations and more dices

--- HelloAndroid/gen/com/example/helloandroid/R.java
+++ HelloAndroid/gen/com/example/helloandroid/R.java
@@ -16,7 +16,7 @@ theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
          */
-        public static final int max=0x7f010001;
+        public static final int color=0x7f010000;
         /** <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
@@ -24,7 +24,7 @@ theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
          */
-        public static final int min=0x7f010000;
+        public static final int max=0x7f010003;
         /** <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
@@ -32,14 +32,26 @@ theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
          */
-        public static final int value=0x7f010002;
+        public static final int min=0x7f010002;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int name=0x7f010001;
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int diceOne=0x7f050000;
-        public static final int diceTwo=0x7f050001;
+        public static final int D10=0x7f050002;
+        public static final int D12=0x7f050001;
+        public static final int D20=0x7f050000;
+        public static final int D4=0x7f050005;
+        public static final int D6=0x7f050004;
+        public static final int D8=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -55,17 +67,33 @@ containing a value of this type.
            <colgroup align="left" />
            <colgroup align="left" />
            <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #DiceView_color com.example.helloandroid:color}</code></td><td></td></tr>
            <tr><td><code>{@link #DiceView_max com.example.helloandroid:max}</code></td><td></td></tr>
            <tr><td><code>{@link #DiceView_min com.example.helloandroid:min}</code></td><td></td></tr>
-           <tr><td><code>{@link #DiceView_value com.example.helloandroid:value}</code></td><td></td></tr>
+           <tr><td><code>{@link #DiceView_name com.example.helloandroid:name}</code></td><td></td></tr>
            </table>
+           @see #DiceView_color
            @see #DiceView_max
            @see #DiceView_min
-           @see #DiceView_value
+           @see #DiceView_name
          */
         public static final int[] DiceView = {
-            0x7f010000, 0x7f010001, 0x7f010002
+            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003
         };
+        /**
+          <p>This symbol is the offset where the {@link com.example.helloandroid.R.attr#color}
+          attribute's value can be found in the {@link #DiceView} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:color
+        */
+        public static final int DiceView_color = 0;
         /**
           <p>This symbol is the offset where the {@link com.example.helloandroid.R.attr#max}
           attribute's value can be found in the {@link #DiceView} array.
@@ -79,7 +107,7 @@ theme attribute (in the form
 containing a value of this type.
           @attr name android:max
         */
-        public static final int DiceView_max = 1;
+        public static final int DiceView_max = 3;
         /**
           <p>This symbol is the offset where the {@link com.example.helloandroid.R.attr#min}
           attribute's value can be found in the {@link #DiceView} array.
@@ -93,20 +121,20 @@ theme attribute (in the form
 containing a value of this type.
           @attr name android:min
         */
-        public static final int DiceView_min = 0;
+        public static final int DiceView_min = 2;
         /**
-          <p>This symbol is the offset where the {@link com.example.helloandroid.R.attr#value}
+          <p>This symbol is the offset where the {@link com.example.helloandroid.R.attr#name}
           attribute's value can be found in the {@link #DiceView} array.
 
 
-          <p>Must be an integer value, such as "<code>100</code>".
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
-          @attr name android:value
+          @attr name android:name
         */
-        public static final int DiceView_value = 2;
+        public static final int DiceView_name = 1;
     };
 }
